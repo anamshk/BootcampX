83008deca0850e8d6c800db06e0ca7c4445e00ed
Anam Shaikh <anam.shk@outlook.com>
2021-11-25 17:57:53 -0500

    parametarize queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,12 +1,24 @@
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+WHERE cohorts.name = $1
+ORDER BY teacher`;
+
+const values = [`%${process.argv[2]}%`];
+
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
